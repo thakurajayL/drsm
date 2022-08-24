5e1f2b0e89799c9ba6cdb7b7d5e45d2d27a265c6
Ajay Lotan Thakur <thakur.ajay@gmail.com>
2022-08-23 23:43:20 -0600

    bson fix

--- updates.go
+++ updates.go
@@ -75,7 +75,9 @@ func punchLiveness(d *Drsm) {
 			filter := bson.M{"_id": "punchLiveness"}
 			//b,_ := json.Marshal(pd)
 			//update := bson.D{{d.clientId.PodName, b}}
-			update := bson.D{{d.clientId.PodName, bson.D{{"podId": d.clientId.PodName}, {"time": time.Now().UnixMilli()}}}}
+			now := time.Now()
+			t := now.UnixMilli()
+			update := bson.D{{d.clientId.PodName, bson.D{{"podId", d.clientId.PodName}, {"time", t}}}}
 
 			_, err = MongoDBLibrary.PutOneCustomDataStructure(d.sharedPoolName, filter, update)
 			if err != nil {
